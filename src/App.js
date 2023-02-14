b4e18599152113fb2c8f794760748b0b0635806f
dev01-srivnworks <dev01.srivnworks@gmail.com>
2023-02-14 11:24:44 +0000

    Let try again.. crating base code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Welcome from './components/home/Welcome';
+
+function App() {
+  return (
+    <>
+    <Welcome></Welcome>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/home/Welcome.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logomd from "../../assets/images/home/srivn.png";
+
+const Welcome = () => {
+  return (
+    <>
+      <div className="sm-align-center">
+        <img src={logomd} class="rounded" alt="..."/>
+          <h2>Lets Keep going...srivn!!</h2>
+        </div>
+    </>
+  );
+};
+
+export default Welcome;
